e0837a27a740e948cec6521e617ba4f34be4df6f
Unknown <Domenico.Iapello@gmail.com>
2017-09-25 15:01:54 +0200

    Adds first Tracefile test and traceFiles with folder

---
+++ src/ndnSIM/examples/ndn-Dome-MoveWithFile-Task.cpp
@@ -0,0 +1,30 @@
+#include "ns3/core-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/ns2-mobility-helper.h"
+#include "ns3/netanim-module.h"
+
+#include "ns3/ndnSIM-module.h"
+
+namespace ns3{
+
+int main (int argc, char *argv[])
+{
+// Create Ns2MobilityHelper for importing ns-2 format mobility trace
+Ns2MobilityHelper ns2 = Ns2MobilityHelper ("/TraceFiles-Dome/ns_movements.txt");
+// Create Moble nodes.
+NodeContainer MobileNodes;
+MobileNodes.Create (2);
+// configure movements for each node, while reading trace file
+ns2.Install ();
+AnimationInterface anim ("SimpleNS3SimulationWithns2-mobility-trace.xml");
+Simulator::Stop (Seconds (100));
+Simulator::Run ();
+return 0;
+}
+}
+
+int
+main(int argc, char* argv[])
+{
+  return ns3::main(argc, argv);
+}
